317b6382f2b7e926710572915afbad9c40abe2aa
Anant-Singh-Chauhan <eng.anant.schauhan21@gmail.com>
2024-01-26 04:22:03 +0530

    Enum for Symbols

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Logger from "./components/Logger";
+import { symbol as PlayerSymbol } from "./commons/Constants";
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
@@ -12,25 +13,23 @@ function App() {
   /// function to derive active player from gameTurns state
   /// : Done to avoid unnecessary state
   ///
-  function deriveActivePlayer(gameTurns){
+  function deriveActivePlayer(gameTurns) {
     // done to ensure change of player,
-      // irrespective of state dependency
-      let selectedPlayer = "X";
+    // irrespective of state dependency
+    let selectedPlayer = PlayerSymbol.X;
 
-      if (gameTurns.length > 0 && gameTurns[0].player === "X") {
-        selectedPlayer = "O";
-      }
+    if (gameTurns.length > 0 && gameTurns[0].player === PlayerSymbol.X) {
+      selectedPlayer = PlayerSymbol.O;
+    }
 
-      return selectedPlayer;
+    return selectedPlayer;
   }
-
 
   ///
   /// function to handle game board inputs
   ///
   function gameBoardInputHandler(rowIndex, colIndex) {
     setGameTurns((prevGameTurns) => {
-      
       let selectedPlayer = deriveActivePlayer(gameTurns);
       let inputGameObj = {
         square: {
@@ -42,8 +41,6 @@ function App() {
       const updatedGameTurns = [inputGameObj, ...prevGameTurns];
       return updatedGameTurns;
     });
-
-    // updateActivePlayer();
   }
 
   return (
@@ -52,8 +49,16 @@ function App() {
 
       {/* -- InfoBar -- */}
       <div className="infoBar">
-        <Player name={"Player-1"} symbol={"X"} isActive={deriveActivePlayer(gameTurns) == "X"} />
-        <Player name={"Player-2"} symbol={"O"} isActive={deriveActivePlayer(gameTurns) == "O"} />
+        <Player
+          name={"Player-1"}
+          symbol={PlayerSymbol.X}
+          isActive={deriveActivePlayer(gameTurns) == PlayerSymbol.X}
+        />
+        <Player
+          name={"Player-2"}
+          symbol={"O"}
+          isActive={deriveActivePlayer(gameTurns) == PlayerSymbol.O}
+        />
       </div>
 
       <div className="game-logger">
---
+++ src/commons/Constants.js
@@ -0,0 +1,6 @@
+
+// Enum to store Player Symbols
+export const symbol = Object.freeze({
+    X: "X",
+    O:"O"
+})
