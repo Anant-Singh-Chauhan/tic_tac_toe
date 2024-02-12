0c1021644029245e6d47f6ba94b7fe952a6e5c32
Anant Singh Chauhan <anant.c@gormalone.com>
2024-02-12 17:32:45 +0530

    remote gameboard start

--- src/App.js
+++ src/App.js
@@ -4,27 +4,30 @@ import "./App.css";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import RemoteMenu from "./components/RemoteMenu";
+import RemoteGame from "./components/RemoteGame";
 
 function App() {
   // socketClient.emit("add-player","");
   const [isLocal, setIsLocal] = useState(undefined);
+  const [showRemoteGameboard, setShowRemoteGameboard] = useState(false);
 
-  function menuHandler(isLocalSelected){
+  function menuHandler(isLocalSelected) {
     setIsLocal(isLocalSelected);
   }
 
   return (
-    
     <div className="App">
       <Header />
 
       {/* Menu and playfield */}
       {isLocal === undefined ? (
-        <Menu menuHandler={menuHandler}/>
+        <Menu menuHandler={menuHandler} />
       ) : isLocal === true ? (
         <LocalGame />
-      ) : (
+      ) : !showRemoteGameboard ? (
         <RemoteMenu />
+      ) : (
+        <RemoteGame />
       )}
     </div>
   );
--- src/components/RemoteMenu.js
+++ src/components/RemoteMenu.js
@@ -43,7 +43,6 @@ export default function RemoteMenu() {
     if(!isPrivateRoom){
         socketClient.emit("add-random-player", playerName);
     };
-    // socketClient.emit("add-remote-player", playerName);
   }
   return (
     <div className="remote-menu">
