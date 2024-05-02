3e172c5e56a9ac6d1e28ff05b0a7cf6bcae53547
Anant Singh Chauhan <anant.c@gormalone.com>
2024-05-02 15:43:38 +0530

    ios bug fix trial

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -3,7 +3,7 @@ export default function GameBoard({ updateGameboard, gameBoard }) {
     <ol className="game-board">
       {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
-          <ol>
+          <ol className="gameboard-row">
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
                 <button
