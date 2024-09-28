096003ffb8f86de39e62e6632cdb92673cb6e82f
Anant-Singh-Chauhan <eng.anant.schauhan21@gmail.com>
2024-09-28 16:52:07 +0530

    env files

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-// const SERVER_URL = "http://localhost:3636";
-const SERVER_URL = "https://tic-tac-toe-server-oo7t.onrender.com";
+const SERVER_URL = process.env.REACT_APP_TIC_TAC_TOE_SERVER_URL;
+
+if(process.env.REACT_APP_DEBUG) console.log(SERVER_URL);
 
 const socketClient = io(SERVER_URL, {
   autoConnect: false,
